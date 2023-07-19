5eb8b70468ce438b2d4d645dfb80d2a606af81d2
kuanchiliao1 <kuanchiliao@gmail.com>
2023-07-19 15:50:53 -0700

    Add error check for negative health ship

--- src/Ship.js
+++ src/Ship.js
@@ -1,17 +1,20 @@
 function Ship(length) {
-  let health = length
+  let health = length;
 
   const isSunk = () => {
+    if (health < 0) {
+      throw new Error('Cannot have negative health!');
+    }
     return health === 0
-  }
+  };
 
   const hit = () => {
-    health -= 1
-  }
+    health -= 1;
+  };
 
   return {
     isSunk,
-    hit
+    hit,
   };
 }
 
