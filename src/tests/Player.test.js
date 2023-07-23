4201627cd85c166ef68e0bccdb69567c96917b00
kuanchiliao1 <kuanchiliao@gmail.com>
2023-07-23 12:00:23 -0700

    Implement setup method and drop one test

--- src/tests/Player.test.js
+++ src/tests/Player.test.js
@@ -1,26 +1,24 @@
 import { createTestScheduler } from 'jest';
 import { Player } from '../Player';
+import { Board } from '../Board';
 
 let human;
 let computer;
 
 beforeEach(() => {
-  human = Player('Human');
-  computer = Player('Computer');
+  const humanBoard = Board.new();
+  const computerBoard = Board.new();
+
+  human = Player('Human', computerBoard);
+  computer = Player('Computer', humanBoard);
 });
 
 test("that human can attack computer's board", () => {
-  'Player can attack';
-
   human.attack([0, 0]);
 });
 
-test("that human can attack computer's board", () => {
-  human.displayEnemyBoard();
-  human.displayBoard();
-});
-
 test('that AI does not shoot same spot twice', () => {
+  computer.attack([0, 0]);
   computer.attack([0, 0]);
   expect(computer.attack([0, 0]).toThrow('Can not hit the same spot twice!'));
 });
