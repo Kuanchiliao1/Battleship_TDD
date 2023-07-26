7f135b0244a47406778a2c65ea95293baeafe3da
kuanchiliao1 <kuanchiliao@gmail.com>
2023-07-25 17:43:43 -0700

    Decompose class into functions

--- src/DOM.js
+++ src/DOM.js
@@ -1,21 +1,19 @@
-function DOM() {
-  function renderBoard(board) {
-    const emojis = board.grid.map((square) => square.state);
+function renderBoard(board) {
+  const emojis = board.grid.map((square) => square.state);
 
-    const boardEl = document.querySelector('.player-board-container');
-    emojis.forEach((emoji) => {
-      const div = document.createElement('div');
-      div.textContent = emoji;
-      div.classList.add('square');
-      boardEl.append(div);
-    });
-  }
+  const playerOneBoardEl = document.querySelector('.player-one-board-container');
+  const playerTwoBoardEl = document.querySelector('.player-two-board-container');
 
-  function bindEventListeners() {
-    
-  }
-
-  return { renderBoard };
+  emojis.forEach((emoji) => {
+    const div = document.createElement('div');
+    div.textContent = emoji;
+    div.classList.add('square');
+    playerOneBoardEl.append(div);
+  });
 }
 
-export { DOM };
+function bindEventListeners() {
+  const boardEl = document.querySelector('.player-board-container');
+}
+
+export { renderBoard, bindEventListeners };
