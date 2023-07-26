89835bfd2988f50d71128a6750b4a35c58a2e86d
kuanchiliao1 <kuanchiliao@gmail.com>
2023-07-26 12:07:15 -0700

    Call methods on Player instead of Board

--- src/index.js
+++ src/index.js
@@ -7,29 +7,31 @@ import { Board } from './Board';
 import { Player } from './Player';
 
 function startGame() {
-  const board1 = Board(10);
-  const board2 = Board(10);
+  const player1 = Player('Test player1', 'one');
+  const player2 = Player('Test player2', 'two');
 
-  board1.placeShip(4, [9, 1], 'vertical');
-  board1.placeShip(3, [3, 0]);
-  board1.placeShip(3, [5, 5]);
+  player1.board.placeShip(4, [9, 1], 'vertical');
+  player1.board.placeShip(3, [3, 0]);
+  player1.board.placeShip(3, [5, 5]);
 
-  const player1 = Player('Test player1');
-  const player2 = Player('Test player2');
+  player2.board.placeShip(3, [5, 5]);
 
   // while (playerOneVictory || playerTwoVictory) {
   //   // alert('working')
   // }
 
-  board1.receiveAttack([0, 0]);
-  board1.receiveAttack([3, 0]);
-  board1.receiveAttack([4, 0]);
-  board1.receiveAttack([5, 0]);
-  board1.receiveAttack([6, 5]);
+  player2.attack([0, 0], player1.board);
+  player2.attack([3, 0], player1.board);
+  player2.attack([4, 0], player1.board);
+  player2.attack([5, 0], player1.board);
+  player2.attack([6, 5], player1.board);
 
-  player2.attack([5, 1], board1);
+  player1.attack([4, 0], player2.board);
+  player1.attack([5, 0], player2.board);
+  player1.attack([6, 5], player2.board);
 
-  renderBoard(board1);
+  renderBoard(player1.board, true, player2.number);
+  renderBoard(player2.board, false, player1.number);
   return 'stuff';
 }
 
