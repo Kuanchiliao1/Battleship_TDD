c85dbd9a0582d5ebe098bf7fe8786a568c056e9c
kuanchiliao1 <kuanchiliao@gmail.com>
2023-07-27 08:06:11 -0700

    Add test or AI autoattack

--- src/tests/Player.test.js
+++ src/tests/Player.test.js
@@ -10,21 +10,35 @@ beforeEach(() => {
 });
 
 test("that human can attack computer's board", () => {
-  human.attack([0, 0]);
+  human.attack([0, 0], computer.board);
   expect(() =>
-    human.attack([0, 0]).toThrow('Can not hit the same spot twice!')
+    human
+      .attack([0, 0], computer.board)
+      .toThrow('Can not hit the same spot twice!')
   );
 });
 
 test('that AI does not shoot same spot twice', () => {
-  computer.attack([0, 0]);
+  computer.attack([0, 0], human.board);
   expect(() =>
-    computer.attack([0, 0]).toThrow('Can not hit the same spot twice!')
+    computer
+      .attack([0, 0], human.board)
+      .toThrow('Can not hit the same spot twice!')
   );
 });
 
 test('that AI can attack by itself', () => {
-  expect(computer.attack());
+  let unhitSquareCount = human.board.grid.filter((square) =>
+    human.board.checkCoordsUnhit(square.coords)
+  ).length;
+
+  computer.attack([], human.board);
+
+  unhitSquareCount = human.board.grid.filter((square) =>
+    human.board.checkCoordsUnhit(square.coords)
+  ).length;
+
+  expect(unhitSquareCount).toBe(99);
 });
 
 // Set up testing to return the attacked coords
