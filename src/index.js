2fbbb9575db52d552f17c4bb9acda88d757a594c
kuanchiliao1 <kuanchiliao@gmail.com>
2023-08-04 09:04:38 -0700

    Refactor and write pseudocode for game loop

--- src/index.js
+++ src/index.js
@@ -1,36 +1,29 @@
 import './styles.css';
 import _ from 'lodash';
-import { Game } from './Game';
 import { bindEventListeners, renderBoard } from './DOM';
 // comment out later..
-import { Board } from './Board';
 import { Player } from './Player';
 
 function startGame() {
+  // prompt player for name
   const player1 = Player('Player');
   const player2 = Player('A.I.');
 
   player1.isCurrentPlayer = true;
 
-  const activePlayer = () => (player1.isCurrentPlayer ? player1 : player2);
-  const switchActivePlayer = () => {
-    player1.isCurrentPlayer = !player1.isCurrentPlayer;
-    player2.isCurrentPlayer = !player2.isCurrentPlayer;
-  };
-  switchActivePlayer()
+  // const activePlayer = () => (player1.isCurrentPlayer ? player1 : player2);
+  // const switchActivePlayer = () => {
+  //   player1.isCurrentPlayer = !player1.isCurrentPlayer;
+  //   player2.isCurrentPlayer = !player2.isCurrentPlayer;
+  // };
 
+  // Let player place all ships(its hardcoded for now)
   player1.board.placeShip(4, [9, 1], 'vertical');
   player1.board.placeShip(3, [3, 0]);
   player1.board.placeShip(3, [5, 5]);
 
   player2.board.placeShip(3, [5, 5]);
   player2.board.placeShip(2, [4, 6]);
-
-  while (false) {
-    activePlayer();
-    alert('game loop is running!');
-    prompt('enter some coordinates');
-  }
 
   player2.attack([0, 0], player1.board);
   player2.attack([3, 0], player1.board);
@@ -49,6 +42,26 @@ function startGame() {
     console.log('player 1 wins!');
   }
   bindEventListeners(player1, player2, renderBoard);
+
+  // Loop until someone's ships are completely sunk. Go one at a time.
+    // While player1+player2.board.checkAllShipsSunk() is false, keep going
+    // Set up logic that allows player to enter coords to attack...
+    //
+  // Ask via prompt first, then integrate into the DOM
+    // player 1 attacks by inputing with prompt()
+    // player1.attack() is called with the inputs
+    // set player1.isCurrentlyActive to false and true for player2
+    // player2 does a random attack with attack()
+  const limit = 20;
+  let i = 0
+  while (!player1.board.checkAllShipsSunk() && !player2.board.checkAllShipsSunk() && i < limit) {
+    // alert('game loop is running!');
+    // prompt('enter some coordinates');
+    i++
+    console.log('loop runs!')
+  }
+
+  
 
   return 'stuff';
 }
