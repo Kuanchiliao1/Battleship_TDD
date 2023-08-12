f2d22ed1bd469f8700faaf2729ff13f747fb4fea
kuanchiliao1 <kuanchiliao@gmail.com>
2023-08-11 19:40:20 -0700

    Remove test attacks

--- src/index.js
+++ src/index.js
@@ -28,15 +28,6 @@ function startGame() {
   player2.board.placeShip(4, [9, 1], 'vertical');
   player2.board.placeShip(3, [3, 0]);
 
-  player2.attack([0, 0], player1.board);
-  player2.attack([3, 0], player1.board);
-  player2.attack([4, 0], player1.board);
-  player2.attack([5, 0], player1.board);
-  player2.attack([6, 5], player1.board);
-
-  player1.attack([4, 0], player2.board);
-  player1.attack([5, 0], player2.board);
-  player1.attack([6, 5], player2.board);
 
   // renderBoard(player2, false);
   // renderBoard(player1, true);
